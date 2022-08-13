dc192a52f823d9034084c688f5b85e0a16b70d4b
sithminbandara <sithminbandara64@gmail.com>
2022-08-13 05:35:54 +0000

    create json response with query parameter

--- main.go
+++ main.go
@@ -17,6 +17,17 @@ func main() {
 		return c.SendString("Hi, user")
 	})
 
+	app.Get("/car", func(c *fiber.Ctx) error {
+		model := c.Query("model")
+		brand := c.Query("brand")
+		year := c.Query("year")
+		return c.JSON(&fiber.Map{
+			"brand": brand,
+			"model": model,
+			"year":  year,
+		})
+	})
+
 	app.Get("/:name", func(c *fiber.Ctx) error {
 		name := c.Params("name")
 		txt := fmt.Sprintf("Hi, %v", name)
