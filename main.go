be9238b6df9575b8d3181423247d03c81af984ea
sithminbandara <sithminbandara64@gmail.com>
2022-08-13 05:29:07 +0000

    add a path parameter

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -13,6 +15,12 @@ func main() {
 
 	app.Get("/user", func(c *fiber.Ctx) error {
 		return c.SendString("Hi, user")
+	})
+
+	app.Get("/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		txt := fmt.Sprintf("Hi, %v", name)
+		return c.SendString(txt)
 	})
 
 	app.Listen(":3000")
